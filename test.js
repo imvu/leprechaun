568d5e4633875c88d4eb9e6a69eecacd7ded8a4c
Andy Friesen <andy@imvu.com>
2012-09-17 16:48:26 -0700

    Tidy test.js

--- test.js
+++ test.js
@@ -10,95 +10,65 @@ if (leprechaun.args.length !== 2) {
     leprechaun.exit(1);
 }
 function onNewBrowser(url) {
-            if (startTime != null) {
-                return;
-            }
-//console.log('building request using url ' + url);
-var req = new XMLHttpRequest();
-req.open('GET', 'http://localhost:24042/json', true);
-req.onerror = function(err) {leprechaun.echo(err);}; 
-req.onabort = function(err) {leprechaun.echo(err);};
-req.onload = function() {
-    //console.log('got response' + req.responseText);
-    var response = JSON.parse(req.responseText);
-    //console.log('this window:' + window.location);
-    for(i =0; i < response.length; i++) {
-    //console.log('opening websocket on ' + response[i].webSocketDebuggerUrl);
-    //console.log('for url:' + response[i].url);
-        if (response[i].webSocketDebuggerUrl == '') {
-            //console.log("skipping debugger connection: null websocket");
-            continue;
-        } else if (response[i].url == window.location) {
-            //console.log("Skipping debugger connection for test runner");
-            continue;
-        }
-    var websocket = new WebSocket(response[i].webSocketDebuggerUrl);
-    webSocketConnections.push(websocket);
-    websocket.onerror = function(e) {leprechaun.echo('got websocket error ' + e.toString());};
-    websocket.onclose = function() {leprechaun.echo('got websocket close');};
-    websocket.onopen = function(evt) {
-        //console.log('WebSocket open worked');
-        //debuggerEnabledCommands.push(commandIndex);
-        //websocket.send(JSON.stringify({id:commandIndex++, method:'Debugger.enable'}));
-        consoleEnabledCommands.push(commandIndex);
-        websocket.send(JSON.stringify({id:commandIndex++, method:'Console.enable'}));
-        //console.log('sent debugger enable');
-    };
-    websocket.onmessage = function(event) {
-        //console.log('got message ' + event.data);
-        var data = JSON.parse(event.data);
-        //var event = JSON.parse(evt);
-        if (data.method === 'Debugger.paused') {
-            //console.log('Got a debugger event!!!!');
-            leprechaun.onError(event.params.reason, event.params.callFrames);
-            return;
-        } else if (data.method === 'Console.messageAdded') {
-            //console.log("got console message: " + data.params.message.text);
-            //console.log("checking console message: " + data.params.message.text.substr(0, 8));
-            if (data.params.message.text.substr(0, 8) === 'Uncaught') {
-                leprechaun.onError(data.params.message.text, data.params.message.stackTrace);
-            } else {
-                leprechaun.onConsoleMessage(data.params.message.text);
-            }
-            return;
-        } else if (data.method === 'Console.messagesCleared') {
-            return;
-        } else if (data.method === 'Debugger.scriptParsed') {
-            return;
-        } else if (-1 !== consoleEnabledCommands.indexOf(data.id)) {
-            //websocket.send(JSON.stringify({id:commandIndex++, method:'Network.enable'}));
-            //websocket.send(JSON.stringify({id:commandIndex++, method:'Timeline.start', params:{'maxCallStackDepth':10}}));
-            w.location= 'http://localhost.imvu.com/jstest/?headless=1&script=' + window.encodeURIComponent(leprechaun.args[1]);
-            startTime = new Date();
-            window.setTimeout(function () {
-                leprechaun.echo('Your unit test took longer than 15 seconds to complete.' +
-                    ' It\'s possible that this test either is absurdly slow, '+
-                    'attempted to access the network, or was unable to load a dependency.');
-            }, 1500);
-            return;
-        /*
-        } else if (-1 !== debuggerEnabledCommands.indexOf(data.id)) {
-            //console.log('Setting debugger to pause on all exceptions');
-            w.location="http://localhost.imvu.com/test.html";
-            //debuggerSetPauseableCommands.push(commandIndex);
-            //websocket.send(JSON.stringify({id:commandIndex++, method:'Debugger.setPauseOnExceptions', params:{state:'all'}}));
-            return;
-        } else if (-1 !== debuggerSetPauseableCommands.indexOf(data.id)) {
-            console.log('response:   ' + event.data);
-            leprechaun.echo('Debugger should pause after excpetions now');
-            startTime = new Date();
-            //var w = window.open("http://localhost.imvu.com/jstest/?script=common/imq/imq.test.js&headless=1");
-            //w.location="http://localhost.imvu.com/test.html";
+    if (startTime != null) {
+        return;
+    }
 
-            return;
-        */
+    var req = new XMLHttpRequest();
+    req.open('GET', 'http://localhost:24042/json', true);
+    req.onerror = function(err) {leprechaun.echo(err);}; 
+    req.onabort = function(err) {leprechaun.echo(err);};
+    req.onload = function() {
+        var response = JSON.parse(req.responseText);
+        for(i =0; i < response.length; i++) {
+            if (response[i].webSocketDebuggerUrl == '') {
+                continue;
+            } else if (response[i].url == window.location) {
+                continue;
+            }
+            var websocket = new WebSocket(response[i].webSocketDebuggerUrl);
+            webSocketConnections.push(websocket);
+            websocket.onerror = function(e) {
+                leprechaun.echo('got websocket error ' + e.toString());
+            };
+            websocket.onclose = function() {
+                leprechaun.echo('got websocket close');
+            };
+            websocket.onopen = function(evt) {
+                consoleEnabledCommands.push(commandIndex);
+                websocket.send(JSON.stringify({id:commandIndex++, method:'Console.enable'}));
+            };
+            websocket.onmessage = function(event) {
+                var data = JSON.parse(event.data);
+                if (data.method === 'Debugger.paused') {
+                    leprechaun.onError(event.params.reason, event.params.callFrames);
+                    return;
+                } else if (data.method === 'Console.messageAdded') {
+                    if (data.params.message.text.substr(0, 8) === 'Uncaught') {
+                        leprechaun.onError(data.params.message.text, data.params.message.stackTrace);
+                    } else {
+                        leprechaun.onConsoleMessage(data.params.message.text);
+                    }
+                    return;
+                } else if (data.method === 'Console.messagesCleared') {
+                    return;
+                } else if (data.method === 'Debugger.scriptParsed') {
+                    return;
+                } else if (-1 !== consoleEnabledCommands.indexOf(data.id)) {
+                    w.location= 'http://localhost.imvu.com/jstest/?headless=1&script=' + window.encodeURIComponent(leprechaun.args[1]);
+                    startTime = new Date();
+                    window.setTimeout(function () {
+                        leprechaun.echo('Your unit test took longer than 15 seconds to complete.' +
+                                        ' It\'s possible that this test either is absurdly slow, '+
+                                        'attempted to access the network, or was unable to load a dependency.');
+                    }, 1500);
+                    return;
+                }
+                leprechaun.echo('Unknown event:' + event.data);
+            };
         }
-        leprechaun.echo('Unknown event:' + event.data);
-    }
-    }
-}
-//console.log('sending json request');
-req.send();
+    };
+    req.send();
 }
 var w = window.open('about:blank');
 
@@ -120,7 +90,7 @@ leprechaun.onConsoleMessage = function onConsoleMessage(msg) {
     } else {
         leprechaun.echo('>>> ' + msg);
     }
-}
+};
 
 leprechaun.onError = function onError(msg, trace) {
     var i, k, fn, file;
@@ -131,5 +101,5 @@ leprechaun.onError = function onError(msg, trace) {
         leprechaun.echo("    at " + fn + " (" + file + ":" + trace[i].lineNumber + ")");
     }
     leprechaun.exit(1);
-}
+};
 
